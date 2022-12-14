d74effe85b5cad3c00e7372dcba7336d1305bc2c
Andolphin6996 <pavlovna2002-2002@mail.ru>
2022-12-14 19:55:49 +0300

    hw07

---
+++ src/test/java/dns/TestBase.java
@@ -0,0 +1,17 @@
+package dns;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import pages.MainPage;
+import pages.SubjectPage;
+
+public class TestBase {
+    MainPage mainPage = new MainPage();
+    SubjectPage subjectPage = new SubjectPage();
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
+}
